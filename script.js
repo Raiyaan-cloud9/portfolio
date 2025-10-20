134d2f37893c7884cd982476c6cb7305d3d7e920
Raiyaan-cloud9 <ghostgains666@gmail.com>
2025-10-20 19:24:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // small friendly greeting (remove before demo if you don't want the popup)
+  // alert("Welcome to Wania's portfolio!");
+  console.log("Portfolio loaded");
+});
